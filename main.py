3022086b4082cca6987aa5eeb62decb50fdbe92b
kerokero0507 <shohhei_mizuguchi@jp.ricoh.com>
2020-03-31 08:25:47 +0900

    [add]読み取り結果のcsv出力機能を追加

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ CONFIGNAME = 'TestData/OcrMapping_TestReport.csv'
 config_file = open(CONFIGNAME)
 config = csv.reader(config_file)
 
-header = next(config)
-
+header = list(range(config.line_num - 1))
 result_list = list(range(config.line_num - 1))
 
+next(config)
 for row in config:
     cut_image = Converter.cutting_out(FILENAME, row[1], row[2], row[3], row[4])
     # cut_image.show()
     result_list.append(Reader.read(cut_image))
+    header.append(row[0])
 
 print(result_list)
+
+output = [header, result_list]
+
+print(output)
+
+outFile = open('TestReport.csv', 'w')
+writer = csv.writer(outFile)
+writer.writerows(output)
