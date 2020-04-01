afd81b78129fce942a8f31e709b539f64590ba9f
kerokero0507 <shohhei_mizuguchi@jp.ricoh.com>
2020-04-01 10:26:04 +0900

    [add]設定機のファイルの読み取り機能を実装 //jsonを使い、読取り先のフォルダおよび出力先のフォルダを指定できる

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import OcrModules.OcrReader as Reader
 import PdfToImage.ImageConverter as Converter
 import csv
+import json
+import FolderControl
+import os
 
-FILENAME = 'TestData/TestReport.pdf'
-CONFIGNAME = 'TestData/OcrMapping_TestReport.csv'
+json_load = json.load(open('config.json', 'r'))
+input_path = json_load['DirectoryConfig']['input']
+output_path = json_load['DirectoryConfig']['output']
 
-config_file = open(CONFIGNAME)
+targets = FolderControl.folders(input_path)
+mapping = FolderControl.sort_files(targets[0], '.csv')[0]
+
+files = FolderControl.sort_files(targets[0], '.pdf')
+
+config_file = open(mapping)
 config = csv.reader(config_file)
 
 header = list(range(config.line_num - 1))
@@ -13,7 +22,7 @@ result_list = list(range(config.line_num - 1))
 
 next(config)
 for row in config:
-    cut_image = Converter.cutting_out(FILENAME, row[1], row[2], row[3], row[4])
+    cut_image = Converter.cutting_out(files[0], row[1], row[2], row[3], row[4])
     # cut_image.show()
     result_list.append(Reader.read(cut_image))
     header.append(row[0])
@@ -24,6 +33,12 @@ output = [header, result_list]
 
 print(output)
 
-outFile = open('TestReport.csv', 'w')
+targets = FolderControl.folders(input_path, True)
+for f in targets:
+    FolderControl.try_mkdir(output_path + '/' + f)
+
+name = os.path.splitext(os.path.basename(files[0]))[0]
+
+outFile = open(output_path + '/' + targets[0] + '/' + name + '.csv', 'w')
 writer = csv.writer(outFile)
 writer.writerows(output)
