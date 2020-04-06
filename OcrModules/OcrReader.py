a797cb92213e23b8e2da7541b1d903d7562cd4a5
kerokero0507 <shohhei_mizuguchi@jp.ricoh.com>
2020-04-06 09:11:54 +0900

    [change]OCR時にページセグメンテーション指定追加 //少数点の数字が読めなかったため、'--psm 6'の指定を追加

--- OcrModules/OcrReader.py
+++ OcrModules/OcrReader.py
@@ -3,9 +3,9 @@ import pytesseract
 
 
 def read(image):
-    return pytesseract.image_to_string(image)
+    return pytesseract.image_to_string(image, config='--psm 6')
 
 
 if __name__ == '__main__':
-    FILENAME = './testimage2.jpg'
-    print(pytesseract.image_to_string(Image.open(FILENAME)))
+    FILENAME = './test_cutimage.png'
+    print(pytesseract.image_to_string(Image.open(FILENAME),config='--psm 6'))
